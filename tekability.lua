0ad5c4c45bfc96ec11ade0ff231413af53c9e4d8
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-06-10 00:41:19 +0000

    tekability - Hrm... forgot to commit this it seems... removed crayon/gratuity use, no external dependancies now
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekability@432 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- tekability.lua
+++ tekability.lua
@@ -2,15 +2,32 @@
 
 local tooltip = tekabilityTooltip
 --~ tekabilityTooltip = nil
-local SLOTTYPES = {"Head", "Shoulder", "Chest", "Waist", "Legs", "Feet", "Wrist", "Hands", "MainHand", "SecondaryHand", "Ranged"}
+local SLOTTYPES, SLOTIDS = {"Head", "Shoulder", "Chest", "Waist", "Legs", "Feet", "Wrist", "Hands", "MainHand", "SecondaryHand", "Ranged"}, {}
+for _,slot in pairs(SLOTTYPES) do SLOTIDS[slot] = GetInventorySlotInfo(slot .. "Slot") end
 local FINDSTRING = string.gsub(DURABILITY_TEMPLATE, "%%[^%s]+", "(.+)")
 local FONTSIZE = 12
 local fontstrings = {}
-local crayon = AceLibrary("Crayon-2.0")
-local gratuity = AceLibrary("Gratuity-2.0")
 local _G = getfenv(0)
 local frame = CreateFrame("Frame")
 local tip = CreateFrame("GameTooltip")
+
+
+local function ColorGradient(perc, ...)
+	if perc >= 1 then
+		local r, g, b = select(select('#', ...) - 2, ...)
+		return r, g, b
+	elseif perc <= 0 then
+		local r, g, b = ...
+		return r, g, b
+	end
+
+	local num = select('#', ...) / 3
+
+	local segment, relperc = math.modf(perc*(num-1))
+	local r1, g1, b1, r2, g2, b2 = select((segment*3)+1, ...)
+
+	return r1 + (r2-r1)*relperc, g1 + (g2-g1)*relperc, b1 + (b2-b1)*relperc
+end
 
 
 for _,slot in ipairs(SLOTTYPES) do
@@ -25,6 +42,16 @@ for _,slot in ipairs(SLOTTYPES) do
 end
 
 
+local function FindDurability()
+	for i=1,tooltip:NumLines() do
+		local str = tooltip.L[i]
+		local _, _, v1, v2 = string.find(str or "", FINDSTRING)
+		if v1 and v2 then return tonumber(v1), tonumber(v2) end
+	end
+	return 0, 0
+end
+
+
 CharacterFrame:HookScript("OnShow", function()
 	frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
 	frame:GetScript("OnEvent")()
@@ -36,30 +63,18 @@ CharacterFrame:HookScript("OnHide", function()
 end)
 
 
-local function FindDurability()
-	for i=1,tooltip:NumLines() do
-		local str = tooltip.L[i]
-		local _, _, v1, v2 = string.find(str or "", FINDSTRING)
-		if v1 and v2 then return tonumber(v1), tonumber(v2) end
-	end
-	return 0, 0
-end
-
-
 frame:SetScript("OnEvent", function()
 	if not CharacterFrame:IsVisible() then return end
 
 	for _,slot in ipairs(SLOTTYPES) do
-		local id, _ = GetInventorySlotInfo(slot .. "Slot")
-		local hasitem = tooltip:SetInventoryItem("player", id)
-		local str = fontstrings[slot]
+		local hasitem, str = tooltip:SetInventoryItem("player", SLOTIDS[slot]), fontstrings[slot]
 
 		if not hasitem then str:SetText("")
 		else
-			local text, v1, v2 = "xxx", FindDurability()
+			local text, v1, v2 = "", FindDurability()
 
 			if v2 ~= 0 then
-				str:SetTextColor(crayon:GetThresholdColor(v1/v2))
+				str:SetTextColor(ColorGradient(v1/v2, 1,0,0, 1,1,0, 0,1,0))
 				text = string.format("%d%%", v1/v2*100)
 			end
 			str:SetText(text)
